fc2382968d5438cd8bdf41a4c0f09f171a3f6f09
maleka <malekabegum1@yahoo.com>
2018-10-04 03:05:54 +0000

    this is a java program

---
+++ AddTwoIntegers.java
@@ -0,0 +1,13 @@
+public class AddTwoIntegers {
+
+    public static void main(String[] args) {
+        
+        int first = 50;
+        int second = 50;
+
+        int sum = first + second;
+
+        System.out.println("This sum is: " + sum);
+    }
+}
+
